78a94b65467b5e2d684633b059e32b3d0f4e36c9
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-09-26 16:52:41 -0400

    create identify_numbers function

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from tabledata import periodic_table_values
 
+
+def identify_numbers(word):
+    numbers = ''
+    for item in word:
+        if item in '1234567890':
+            numbers += item
+        else:
+            pass
+    
+    return int(numbers)
+
+
+
+
 mass = 0
 while True:
     print("Enter the element and the a full stop to end the compound of molecule (case sensitive) ")
